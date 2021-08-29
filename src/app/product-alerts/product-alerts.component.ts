cccf66a8826c9bd2a2d8f6f3747f2a10d8616d2b
TanmayNimbarte <tanmaynimbarte311@gmail.com>
2021-08-29 20:17:48 +0530

    2 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
+// @Component(
+//   {
+//     selector: 'app-pac',
+//     templateUrl: './pac.component.html',
+//     styleUrls: ['./pac.component.css']
+//   }
+// )
+// export class PAC{
+  
+// }
